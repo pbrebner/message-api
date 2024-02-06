903455a02ed33dbf119ac584cbf269a278ee0081
Patrick Brebner <patrick.brebner@gmail.com>
2024-02-05 18:39:05 -0800

    Add api router

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const logger = require("morgan");
 const cors = require("cors");
 
 const indexRouter = require("./routes/index");
+const apiRouter = require("./routes/api");
 
 const app = express();
 app.use(cors());
@@ -35,6 +36,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
+app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
