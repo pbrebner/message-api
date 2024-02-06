e82fd39889199c8209e27ba780fc0843d49ed9bb
Patrick Brebner <patrick.brebner@gmail.com>
2024-02-05 18:39:19 -0800

    Update to redirect to api

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-    console.log("Nothin Implemented");
+    res.redirect("/api");
 });
 
 module.exports = router;
