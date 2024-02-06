c2f5ba8ac719b84ecc5fe827cbfce3385cd0e43f
Patrick Brebner <patrick.brebner@gmail.com>
2024-02-06 14:59:36 -0800

    Add verify token as middleware

--- routes/api.js
+++ routes/api.js
@@ -17,49 +17,89 @@ router.post("/login", authController.login);
 
 // USER ROUTES
 
-router.get("/users", userController.getAllUsers);
+router.get("/users", authController.verifyToken, userController.getAllUsers);
 
 router.post("/users", userController.createUser);
 
-router.get("/users/:userId", userController.getUser);
+router.get(
+    "/users/:userId",
+    authController.verifyToken,
+    userController.getUser
+);
 
-router.put("users/:userId", userController.updateUser);
+router.put(
+    "users/:userId",
+    authController.verifyToken,
+    userController.updateUser
+);
 
-router.delete("users/:userId", userController.deleteUser);
+router.delete(
+    "users/:userId",
+    authController.verifyToken,
+    userController.deleteUser
+);
 
 // CHANNEL ROUTES
 
-router.get("/channels", channelController.getAllUserChannels);
+router.get(
+    "/channels",
+    authController.verifyToken,
+    channelController.getAllUserChannels
+);
 
-router.post("/channels", channelController.createChannel);
+router.post(
+    "/channels",
+    authController.verifyToken,
+    channelController.createChannel
+);
 
-router.get("/channels/:channelId", channelController.getChannel);
+router.get(
+    "/channels/:channelId",
+    authController.verifyToken,
+    channelController.getChannel
+);
 
-router.put("/channels/:channelId", channelController.updateChannel);
+router.put(
+    "/channels/:channelId",
+    authController.verifyToken,
+    channelController.updateChannel
+);
 
-router.delete("/channels/:channelId", channelController.deleteChannel);
+router.delete(
+    "/channels/:channelId",
+    authController.verifyToken,
+    channelController.deleteChannel
+);
 
 // MESSAGE ROUTES
 
 router.get(
     "/channels/:channelId/messages",
+    authController.verifyToken,
     messageController.getAllChannelMessages
 );
 
-router.post("/channels/:channelId/messages", messageController.createMessage);
+router.post(
+    "/channels/:channelId/messages",
+    authController.verifyToken,
+    messageController.createMessage
+);
 
 router.get(
     "/channels/:channelId/messages/:messageId",
+    authController.verifyToken,
     messageController.getMessage
 );
 
 router.put(
     "/channels/:channelId/messages/:messageId",
+    authController.verifyToken,
     messageController.updateMessage
 );
 
 router.delete(
     "/channels/:channelId/messages/:messageId",
+    authController.verifyToken,
     messageController.deleteMessage
 );
 
