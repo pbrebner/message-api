71348ecc36f67264f7c93d6c9394d9013be437f3
Patrick Brebner <patrick.brebner@gmail.com>
2024-02-05 17:34:33 -0800

    Add routes

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const authController = require("../controllers/authController");
+const userController = require("../controllers/userController");
+const messageController = require("../controllers/messageController");
+const channelController = require("../controllers/channelController");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -13,3 +16,51 @@ router.get("/", function (req, res, next) {
 router.post("/login", authController.login);
 
 // USER ROUTES
+
+router.get("/users", userController.getAllUsers);
+
+router.post("/users", userController.createUser);
+
+router.get("/users/:userId", userController.getUser);
+
+router.put("users/:userId", userController.updateUser);
+
+router.delete("users/:userId", userController.deleteUser);
+
+// CHANNEL ROUTES
+
+router.get("/channels", channelController.getAllUserChannels);
+
+router.post("/channels", channelController.createChannel);
+
+router.get("/channels/:channelId", channelController.getChannel);
+
+router.put("/channels/:channelId", channelController.updateChannel);
+
+router.delete("/channels/:channelId", channelController.deleteChannel);
+
+// MESSAGE ROUTES
+
+router.get(
+    "/channels/:channelId/messages",
+    messageController.getAllChannelMessages
+);
+
+router.post("/channels/:channelId/messages", messageController.createMessage);
+
+router.get(
+    "/channels/:channelId/messages/:messageId",
+    messageController.getMessage
+);
+
+router.put(
+    "/channels/:channelId/messages/:messageId",
+    messageController.updateMessage
+);
+
+router.delete(
+    "/channels/:channelId/messages/:messageId",
+    messageController.deleteMessage
+);
+
+module.exports = router;
