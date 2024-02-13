e4905dcc1cd2285c8d2c7f7bc2cf6353d42da1f1
Patrick Brebner <patrick.brebner@gmail.com>
2024-02-12 17:04:06 -0800

    Add avatar, friends, and online attributes

--- models/user.js
+++ models/user.js
@@ -6,8 +6,15 @@ const userSchema = new Schema({
     email: { type: String, required: true },
     password: { type: String, required: true },
     bio: { type: String, default: "" },
+    avatar: {
+        type: String,
+        default:
+            "https://blog-bucket-banana.s3.us-west-2.amazonaws.com/blogProfileDefault.png",
+    },
     memberStatus: { type: Boolean, default: false },
+    friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
     channels: [{ type: Schema.Types.ObjectId, ref: "Channel" }],
+    online: { type: Boolean, default: false },
     timeStamp: { type: Date, default: Date.now, required: true },
 });
 
