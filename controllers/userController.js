267b6b1a94cd521e9fc40f34dd4d8062ddbd0d7b
Patrick Brebner <patrick.brebner@gmail.com>
2024-02-20 12:39:49 -0800

    Update friends in user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const Friend = require("../models/friend");
 const Channel = require("../models/channel");
 const Message = require("../models/message");
 
@@ -12,11 +13,7 @@ exports.getAllUsers = asyncHandler(async (req, res, next) => {
         "name avatar memberStatus timeStamp"
     ).exec();
 
-    if (!users) {
-        res.status(404).json({ error: "No entries found in database." });
-    } else {
-        res.json(users);
-    }
+    res.json(users);
 });
 
 exports.createUser = [
@@ -39,7 +36,7 @@ exports.createUser = [
         .isEmail()
         .withMessage("Email is not proper email format.")
         .custom(async (value) => {
-            const user = await User.find({ username: value }).exec();
+            const user = await User.find({ email: value }).exec();
             if (user.length > 0) {
                 throw new Error(
                     "Email is already in use, please use a different one."
@@ -79,7 +76,10 @@ exports.createUser = [
                     });
                 } else {
                     await user.save();
-                    res.json({ message: "User successfully created." });
+                    res.json({
+                        userId: user._id,
+                        message: "User successfully created.",
+                    });
                 }
             }
         });
@@ -93,7 +93,7 @@ exports.getUser = asyncHandler(async (req, res, next) => {
             { _id: req.user._id },
             "name email bio avatar memberStatus friends channels timeStamp"
         )
-            .populate("friends", { name: 1, bio: 1, avatar: 1, online: 1 })
+            .populate("friends", { targetUser: 1, status: 1, timeStamp: 1 })
             .populate("channels")
             .exec();
 
@@ -145,7 +145,7 @@ exports.updateUser = [
         .withMessage("Email is not proper email format.")
         .custom(async (value, { req }) => {
             const currentUser = await User.findById(req.user._id).exec();
-            const user = await User.find({ username: value }).exec();
+            const user = await User.find({ email: value }).exec();
 
             if (user.length > 0) {
                 // Check if email is users own
@@ -164,18 +164,6 @@ exports.updateUser = [
         .optional()
         .blacklist("<>"),
     body("avatar").optional().trim(),
-    body("friends", "Have to specify a user")
-        .notEmpty()
-        .optional()
-        .custom(async (value, { req }) => {
-            const user = await User.find({ name: value }).exec();
-
-            if (!user) {
-                throw new Error(`No user with ${value} exists.`);
-            } else {
-                req.body.friendUpdate = user._id;
-            }
-        }),
     body("online").optional(),
     asyncHandler(async (req, res, next) => {
         //Confirm user is updating their own account
@@ -194,36 +182,14 @@ exports.updateUser = [
                     errors: errors.array(),
                 });
             } else {
-                const friendList = user.friends;
-
-                // Prepare friendList if required and update friendUpdate's friends
-                if (req.body.friends) {
-                    if (friendList.includes(req.body.friendUpdate)) {
-                        // Remove friend
-                        friendList = friendList.filter(
-                            (friend) => friend != req.body.friendUpdate
-                        );
-                        await User.findByIdAndUpdate(req.body.friendUpdate, {
-                            $pull: { friends: req.user._id },
-                        });
-                    } else {
-                        // Add friend
-                        friendList.push(req.body.friendUpdate);
-                        await User.findByIdAndUpdate(req.body.friendUpdate, {
-                            $push: { friends: req.user._id },
-                        });
-                    }
-                }
-
                 // Update User
                 const updatedUser = await User.findByIdAndUpdate(req.user._id, {
                     name: req.body.name || user.name,
                     email: req.body.email || user.email,
                     bio: req.body.bio || user.bio,
                     avatar: req.body.avatar || user.avatar,
-                    friends: friendList,
                     online: req.body.online || user.online,
-                });
+                }).exec();
 
                 res.json({
                     userId: updatedUser._id,
@@ -242,34 +208,46 @@ exports.updateUser = [
 exports.deleteUser = asyncHandler(async (req, res, next) => {
     //Confirm user is deleting their own account
     if (req.user._id === req.params.userId) {
-        const user = await User.findByIdAndDelete(req.user._id);
+        const user = await User.findByIdAndDelete(req.user._id).exec();
 
         if (!user) {
             return res
                 .status(404)
                 .json({ error: `No user with id ${req.user._id} exists` });
         } else {
+            // Delete Friends
+            await Friend.deleteMany({ user: req.user._id });
+            const friends = await Friend.deleteMany({
+                userTarget: req.user._id,
+            }).exec();
+            friends.forEach(async (friend) => {
+                await User.findByIdAndUpdate(friend.user, {
+                    $pull: { friends: friend._id },
+                }).exec();
+            });
+
+            // Delete channels if user was one of two users
             const channels = await Channel.find(
                 { users: { $in: req.user._id } },
                 "users"
-            );
-
-            // Delete channel if user was one of two users
+            ).exec();
             channels.forEach(async (channel) => {
                 if (channel.users.length == 2) {
-                    await Channel.findByIdAndDelete(channel._id);
+                    await Channel.findByIdAndDelete(channel._id).exec();
                 } else {
                     await Channel.findByIdAndUpdate(channel._id, {
                         $pull: {
                             users: req.user._id,
                             messages: { user: req.user._id },
                         },
-                    });
+                    }).exec();
                 }
             });
 
             // Delete all messages
-            const messages = await Message.deleteMany({ user: req.user._id });
+            await Message.deleteMany({
+                user: req.user._id,
+            }).exec();
 
             res.json({
                 message: "User deleted successfully.",
