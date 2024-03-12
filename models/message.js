9b1b16bf208bae6b0fd4df896a30a14e806a24ab
Patrick Brebner <patrick.brebner@gmail.com>
2024-03-12 16:23:37 -0700

    Add posted to message model

--- models/message.js
+++ models/message.js
@@ -9,6 +9,7 @@ const messageSchema = new Schema({
     },
     inResponseTo: { type: Schema.Types.ObjectId, ref: "Message" },
     likes: { type: Number, default: 0 },
+    posted: { type: Boolean, required: true },
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
     channel: { type: Schema.Types.ObjectId, ref: "Channel", required: true },
     timeStamp: { type: Date, default: Date.now, required: true },
