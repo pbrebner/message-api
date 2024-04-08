7acb9e3667a37baa4434896f579c73ef13f019b3
Patrick Brebner <patrick.brebner@gmail.com>
2024-04-07 17:22:20 -0700

    Update Socket

--- socketServer.js
+++ socketServer.js
@@ -17,31 +17,53 @@ function setSocketServer(app) {
 
     io.on("connection", (socket) => {
         console.log("A user connected");
-        // TODO: Need to check if logged in and set online status accordingly
 
-        // After successful log in
-        socket.on("loggedIn", async (data) => {
+        // Sets user status to online
+        socket.on("online", async (data) => {
+            console.log("User Online");
             socket.data.userId = data.userId;
+
             await User.findByIdAndUpdate(data.userId, { online: true });
+            socket.join(data.userId);
+
+            // TODO: Maybe create room with all friends instead
+            socket.emit("receiveOnline");
+            console.log(socket.rooms);
         });
 
+        // Sets user status to offline
+        socket.on("offline", async (data) => {
+            console.log("User Offline");
+            await User.findByIdAndUpdate(data.userId, { online: false });
+            socket.leave(data.userId);
+            console.log(socket.rooms);
+        });
+
+        // On socket disconnect
         socket.on("disconnect", async () => {
-            console.log("user disconnected");
+            console.log("User disconnected");
             await User.findByIdAndUpdate(socket.data.userId, { online: false });
+            socket.leave(socket.data.userId);
+            console.log(socket.rooms);
         });
 
         // Join Room (channel)
         socket.on("joinChannel", (data) => {
             socket.join(data.room);
+            console.log(`Joined channel ${data.room}`);
+            console.log(socket.rooms);
         });
 
         // Leave Room (channel)
         socket.on("leaveChannel", (data) => {
             socket.leave(data.room);
+            console.log(`Left channel ${data.room}`);
+            console.log(socket.rooms);
         });
 
         // Send message to channel
         socket.on("sendMessage", (data) => {
+            console.log("Sending message");
             socket.to(data.room).emit("receiveMessage");
         });
     });
