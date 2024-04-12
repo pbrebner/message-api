23f8e1a9f2f9f459bdac292d00d369fc09d71017
Patrick Brebner <patrick.brebner@gmail.com>
2024-04-11 17:24:53 -0700

    Add logic to update channels

--- socketServer.js
+++ socketServer.js
@@ -26,8 +26,8 @@ function setSocketServer(app) {
             await User.findByIdAndUpdate(data.userId, { online: true });
             socket.join(data.userId);
 
-            // TODO: Maybe create room with all friends instead
-            socket.emit("receiveOnline");
+            // TODO: Maybe create room with all friends instead to emit to
+            io.to(data.userId).emit("receiveOnline");
             console.log(socket.rooms);
         });
 
@@ -47,24 +47,63 @@ function setSocketServer(app) {
             console.log(socket.rooms);
         });
 
-        // Join Room (channel)
-        socket.on("joinChannel", (data) => {
+        // Join Room (channel._id)
+        socket.on("joinRoom", (data) => {
             socket.join(data.room);
-            console.log(`Joined channel ${data.room}`);
+            console.log(`Joined Room ${data.room}`);
             console.log(socket.rooms);
         });
 
-        // Leave Room (channel)
-        socket.on("leaveChannel", (data) => {
+        // Join Rooms to receive updates on channel
+        socket.on("joinRooms", (data) => {
+            socket.join(data.rooms);
+            console.log(socket.rooms);
+        });
+
+        // Leave Room (channel._id)
+        socket.on("leaveRoom", (data) => {
             socket.leave(data.room);
-            console.log(`Left channel ${data.room}`);
+            console.log(`Left Room ${data.room}`);
             console.log(socket.rooms);
         });
 
-        // Send message to channel
-        socket.on("sendMessage", (data) => {
-            console.log("Sending message");
-            socket.to(data.room).emit("receiveMessage");
+        // Create Channel
+        socket.on("createChannel", (data) => {
+            console.log("Creating channel");
+
+            // Emit to each user id
+            data.users.forEach((userId) => {
+                io.to(userId).emit("receiveChannelCreate");
+            });
+        });
+
+        // Update Channel
+        socket.on("updateChannel", (data) => {
+            console.log("Updating channel");
+            io.to(data.room).emit("receiveChannelUpdate");
+
+            // If adding new users
+            if (data.users) {
+                // Emit to each user id
+                data.users.forEach((userId) => {
+                    io.to(userId).emit("receiveChannelCreate");
+                });
+            }
+        });
+
+        // Delete Channel
+        socket.on("deleteChannel", (data) => {
+            console.log("Deleting channel");
+            io.to(data.room).emit("receiveChannelUpdate");
+
+            // Make all socket instances leave the specified room
+            io.socketsLeave(data.room);
+        });
+
+        // Update Message (Send, Update, Delete) in Channel
+        socket.on("updateMessage", (data) => {
+            console.log("Updating message");
+            io.to(data.room).emit("receiveMessageUpdate");
         });
     });
 
